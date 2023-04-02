e79f8444e4b8b340741be98d313a1b7462b90897
DreamHealer <swehtc@gmail.com>
2023-04-02 08:40:12 +0000

    ...

--- index.php
+++ index.php
@@ -45,29 +45,25 @@ echo "    </div>\n";
 echo "    <div id='content' class='container-fluid'>\n";
 echo "      <div id='content-main' class='container'>\n";
 echo "        <h2 style='color:#fbb204'>GameAnalyzer</h2>\n";
-echo "        <p style='font-size:14px'>GameAnalyzer is a tool I made to more easilly find interesting situations in demos</p>\n";
+echo "        <p style='font-size:14px'>GameAnalyzer is a tool I made to more easilly identify and list interesting situations in demos</p>\n";
 
 echo "        <div id='content-main-inner' class='row'>\n";
 echo "          <div id='content-left' class='col col-2'>\n";
-echo "              <h4 style='color:#fbb204'>Poäng:</h4>\n";
+echo "              <h4 style='color:#fbb204'></h4>\n";
 echo "              <ul>\n";
-
-echo "              </ul>\n";
-echo "              <h4 style='color:#fbb204'>Kommandon:</h4>\n";
-echo "              <ul>\n";
-echo "                  <li>!ktop - visa top10</li>\n";
 echo "              </ul>\n";
 echo "          </div>\n";
 
 echo "          <div id='content-middle' class='col col-6'>\n";
-echo "            <h4 style='color:#fbb204'>Senaste händelserna</h4>\n";
+echo "            <h4 style='color:#fbb204'>Latest events</h4>\n";
 echo "             <table id='eventlist' class='table table-bordered' style='background-color: #FAFAFA;'>\n";
 
 echo "              <thead class='table-dark'>\n";
 echo "                <tr>\n";
 echo "                  <th class='col-3'>Time</th>\n";
-echo "                  <th class='col-4'>Attacker</th>\n";
-echo "                  <th class='col-4'>Victim</th>\n";
+echo "                  <th class='col-3'>Attacker</th>\n";
+echo "                  <th class='col-3'>Victim</th>\n";
+echo "                  <th class='col-3'>Information</th>\n";
 echo "                </tr>\n";
 echo "              </thead>\n";
 
@@ -83,12 +79,14 @@ foreach ( $eList->getArray() as $event ) {
     echo "                <tr ".$rowClass.">\n";
     echo "                  <td class='col-3'>".$event->getStamp()."</td>\n";
     if ( $isTeamKill ) {
-        echo "                  <td class='col-4'>".$event->getAttacker()." [-".$event->getPoints()."p]</td>\n";
-        echo "                  <td class='col-4'>".$event->getVictim()." [+".$event->getPoints()."p]</td>\n";    
+        echo "                  <td class='col-3'>".$event->getAttacker()." [-".$event->getPoints()."p]</td>\n";
+        echo "                  <td class='col-3'>".$event->getVictim()." [+".$event->getPoints()."p]</td>\n";    
     } else {
-        echo "                  <td class='col-4'>".$event->getAttacker()." [+".$event->getPoints()."p]</td>\n";
-        echo "                  <td class='col-4'>".$event->getVictim()."</td>\n";    
+        echo "                  <td class='col-3'>".$event->getAttacker()." [+".$event->getPoints()."p]</td>\n";
+        echo "                  <td class='col-3'>".$event->getVictim()."</td>\n";    
     }
+    echo "                  <td class='col-3'>".$event->getInfo()."</td>\n";    
+
     echo "                </tr>\n";
 }
 
@@ -96,39 +94,6 @@ echo "              </tbody>\n";
 echo "            </table>\n";
 echo "          </div>\n";
 
-/* CONTENT RIGHT */
-echo "          <div id='content-right' class='col col-3'>\n";
-echo "            <h4 style='color:#fbb204'>Topplista</h4>\n";
-echo "            <table id='userlist' class='table table-bordered' style='background-color: #FAFAFA;'>\n";
-
-echo "              <thead class='table-dark '>\n";
-echo "                <tr>\n";
-echo "                  <th class='col-1'>Rank</th>\n";
-echo "                  <th class='col-4'>Name</th>\n";
-echo "                  <th class='col-2'>Points</th>\n";
-echo "                </tr>\n";
-echo "              </thead>\n";
-
-echo "              <tbody>\n";
-
-$uList = getUserListSorted ( );
-$index = 0;
-foreach ( $uList->getArray() as $user ) {
-    echo "                <tr>\n";
-    echo "                  <td class='col-1'>#".++$index."</td>\n";
-    echo "                  <td class='col-4'>".$user->getName()."</td>\n";
-    echo "                  <td class='col-2'>".$user->getPoints()."</td>\n";
-    echo "                </tr>\n";
-}
-
-echo "              </tbody>\n";
-echo "            </table>\n";
-
-echo "          </div>\n";
-echo "        </div>\n";
-echo "      </div>\n";
-
-echo "    </div>\n";
 
 /* FOOTER */ 
 echo "    <div id='footer'>\n";
