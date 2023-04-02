f26551fbe723ee548d921b251281118e38de77d8
DreamHealer <swehtc@gmail.com>
2023-04-02 08:51:15 +0000

    ...

--- class/functions.php
+++ class/functions.php
@@ -5,13 +5,13 @@ include_once 'arraylist.php';
 function getEventList ( ) {
     global $mysql;
     $eList = new ArrayList ( );
-    $query = "SELECT stamp,attacker,victim,info,type FROM event ORDER BY stamp DESC";
+    $query = "SELECT stamp,killer,victim,info,type FROM event ORDER BY stamp DESC";
     $stmt = $mysql->prepare ( $query ) or die ( "Error: " . $mysql->getError ( ) );
     $stmt->execute ( ) or die ( "Error: " . $mysql->getError ( ) );
     $stmt->store_result ( );
-    $stmt->bind_result ( $stamp, $attacker, $victim, $info ) or die ( "Error: " . $mysql->getError ( ) );
+    $stmt->bind_result ( $stamp, $killer, $victim, $info ) or die ( "Error: " . $mysql->getError ( ) );
     while ( $stmt->fetch ( ) ) {
-        $eList->add ( new Event ( $stamp, $attacker, $victim, $info ) );
+        $eList->add ( new Event ( $stamp, $killer, $victim, $info ) );
     }
     return $eList;
 }
